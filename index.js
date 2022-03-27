619b48a238c3a97c7545d99236c47740cdbde1ff
Aiswaryaraja1996 <63009970+Aiswaryaraja1996@users.noreply.github.com>
2022-03-27 21:57:04 +0530

    added events

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 //* http
 
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req, res) => {
-  try {
-    res.writeHead(200, { contentType: "application/json" });
-    res.end(JSON.stringify({ data: "Hello World!!" }));
-  } catch (err) {
-    res.writeHead(400, { contentType: "application/json" });
-    res.end(JSON.stringify({ data: "Error!!" }));
-  }
-});
-
-server.listen(3001, () => {
-  console.log("Listening to port 3001...");
-});
-
-//* Events
-
-// const EventEmitter = require("events");
-
-// const emitter = new EventEmitter();
-// let a = 1;
-
-// emitter.on("message", (a) => {
-//   if (a === 10) {
-//     console.log("Exiting!!");
-//     process.exit();
-//   } else {
-//     console.log("Message " + a);
+// const server = http.createServer((req, res) => {
+//   try {
+//     res.writeHead(200, { contentType: "application/json" });
+//     res.end(JSON.stringify({ data: "Hello World!!" }));
+//   } catch (err) {
+//     res.writeHead(400, { contentType: "application/json" });
+//     res.end(JSON.stringify({ data: "Error!!" }));
 //   }
 // });
 
-// setInterval(() => {
-//   emitter.emit("message", a++);
-// }, 1000);
+// server.listen(3001, () => {
+//   console.log("Listening to port 3001...");
+// });
+
+//* Events
+
+const EventEmitter = require("events");
+
+const emitter = new EventEmitter();
+let a = 1;
+
+emitter.on("message", (a) => {
+  if (a === 10) {
+    console.log("Exiting!!");
+    process.exit();
+  } else {
+    console.log("Message " + a);
+  }
+});
+
+setInterval(() => {
+  emitter.emit("message", a++);
+}, 1000);
 
 //* Process
 // Boiler Plate code of OJ
